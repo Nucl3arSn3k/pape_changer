869c51b66c4cd326f94cf5be4bede59e80660999
Nucl3arSn3k <shredderofwheat@gmail.com>
2026-04-14 20:37:29 -0400

    Update .gitignore, Cargo.lock, Cargo.toml, and 2 more files

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use std::env;
+
+
+mod papechange;
+fn main() {
+    //let args: Vec<String> = env::args().collect();
+
+    papechange::changepape();
+    //match on args
+}
---
+++ src/papechange.rs
@@ -0,0 +1,49 @@
+use rand::{Rng, RngExt, rng};
+use dbus::blocking::Connection;
+use std::time::Duration;
+pub fn changepape() {
+    let entries: Vec<_> = std::fs::read_dir("/home/quinton/Pictures/img/papes") //Mod to dummy out dirs lists them in toplevel. muy no bueno
+    .expect("Failed to read directory")
+    .filter_map(|e| e.ok())
+    .map(|e| e.path())
+    .collect();
+
+    if entries.is_empty() { return; }
+
+    let idx = rng().random_range(0..entries.len());
+    let pape = &entries[idx];
+    set_wallpaper(pape.to_str().unwrap());
+    println!("{}", pape.display());
+}
+
+
+
+fn set_wallpaper(path: &str) { //Set and remove somehow
+    let conn = Connection::new_session().expect("Failed to connect to DBus");
+    let proxy = conn.with_proxy(
+        "org.kde.plasmashell",
+        "/PlasmaShell",
+        Duration::from_millis(5000),
+    );
+    //JS for internal scripting,EWWWWW
+    let script = format!(
+        r#"
+        var allDesktops = desktops();
+    for (var i = 0; i < allDesktops.length; i++) {{
+        var d = allDesktops[i];
+        d.wallpaperPlugin = 'org.kde.image';
+    d.currentConfigGroup = ['Wallpaper', 'org.kde.image', 'General'];
+    d.writeConfig('Image', 'file://{}');
+}}
+"#,
+path
+    );
+
+    proxy.method_call::<(), _, _, _>("org.kde.PlasmaShell", "evaluateScript", (script,))
+    .expect("Failed to set wallpaper");
+}
+
+//So the wallpaper is kept in ~/.config/plasma-org.kde.plasma.desktop-appletsrc file
+//get full pwd for that and then see if I can snipe it out of recents while leaving it set....hrmm
+//
+
