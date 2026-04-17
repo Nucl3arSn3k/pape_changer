dc2a80dbb3efc94ff028e5d580e9d47fe273a558
Nucl3arSn3k <shredderofwheat@gmail.com>
2026-04-17 05:49:01 -0400

    added skeleton of function

--- src/papechange.rs
+++ src/papechange.rs
@@ -50,4 +50,12 @@ path
 //So the wallpaper is kept in ~/.config/plasma-org.kde.plasma.desktop-appletsrc file
 //get full pwd for that and then see if I can snipe it out of recents while leaving it set....hrmm
 //
+#[cfg(target_os = "linux)]
+fn pape_amnesia() {
 
+
+
+
+
+
+}
